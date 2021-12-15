e228ecf0fc7dac513320941669dddbb7de4e7280
EALucero <ea.lucero@outlook.com>
2021-12-15 13:53:14 -0300

    rev

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatar from '../assets/images/jordan-walke.png'
+import avatar from '../assets/images/jordan-walke.png';
 
 export default function NavBar () {
     return (
